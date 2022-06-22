447f076af7aa0b0c723a7a979541c03533738f12
danylosysyn <93543494+danylosysyn@users.noreply.github.com>
2022-06-22 20:43:35 +0300

    added a spinner

--- my-app/src/components/MainPage/MainPage.jsx
+++ my-app/src/components/MainPage/MainPage.jsx
@@ -6,7 +6,7 @@ import "../../firebase/firebase"
 import { useDispatch, useSelector } from "react-redux";
 import { selectInfo } from "../../redux/selectors/selectInfo"
 import { getInfo } from "../../redux/actions/infoActions";
-
+import { Spinner } from "react-bootstrap";
 
 const MainPage = () => {
     return (
@@ -25,21 +25,36 @@ const InfoDiv = () => {
         getInfoThunk();
     }, [dispatch]);
     const info = useSelector(selectInfo);
+    useEffect(() => {
+        let loadingSpinner = document.getElementById("loadingSpinner");
+        let allInfo = document.getElementById("allInfo");
+        console.log(info)
+        if (info === undefined) {
+            allInfo.style.display = "none"
+            loadingSpinner.style.display = "block"
+        } else {
+            allInfo.style.display = "block"
+            loadingSpinner.style.display = "none"
+        }
+    }, [info]);
     return (
         <div id="wholeDiv">
-            <div id="infoDiv">
-                <div id="myInfo">
-                    <p id="nameText">{info?.name} {info?.surname}</p>
-                    <h3>{info?.city}, {info?.country}</h3>
-                    <h3>{info?.job}</h3>
+            <Spinner animation="grow" id="loadingSpinner" variant="danger"/>
+            <div id="allInfo">
+                <div id="infoDiv">
+                    <div id="myInfo">
+                        <p id="nameText">{info?.name} {info?.surname}</p>
+                        <h3>{info?.city}, {info?.country}</h3>
+                        <h3>{info?.job}</h3>
+                    </div>
+                    <div id="imageDiv">
+                        <img src={info?.image} alt="Loading..." />
+                    </div>
                 </div>
-                <div id="imageDiv">
-                    <img src={info?.image} alt="Loading..." />
+                <div id="secondInfoDiv">
+                    <h2 style={{ fontWeight: 600 }}> About me:</h2>
+                    <p id="myInfoText" style={{ whiteSpace: "pre-line" }}>{info?.extraInfo}</p>
                 </div>
-            </div>
-            <div id="secondInfoDiv">
-                <h2 style={{ fontWeight: 600 }}> About me:</h2>
-                <p id="myInfoText" style={{whiteSpace: "pre-line"}}>{info?.extraInfo}</p>
             </div>
         </div>
     )
